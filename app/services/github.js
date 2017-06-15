88e348648baaa2941d8eaf93e668a8c50efe838b
Marcus Sanatan <msanatan@gmail.com>
2017-06-15 19:49:37 -0400

    Add GitHub service This file contains all the interactions with the GitHub API. It's first function simply returns the list of repositories given a username

---
+++ app/services/github.js
@@ -0,0 +1,25 @@
+const handleErrors = (response) => {
+  if (!response.ok) {
+    if (response.message) {
+      throw Error(response.message);
+    }
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
+const getRepos = (username) => {
+  let name = username.trim().toLowerCase();
+  if (name === '') {
+    return [];
+  }
+  return fetch(`https://api.github.com/users/${name}/repos`)
+    .then(handleErrors)
+    .then((response) => {return response.json()})
+    .catch((error) => {
+      console.error(error);
+    })
+};
+
+export { getRepos };
+
