e2ffd8844f95be0aa13f21e1637457776349e0da
Marcus Sanatan <msanatan@gmail.com>
2017-06-15 19:06:11 -0400

    Add placeholder for list of user repositories

---
+++ app/scenes/SearchScreen/components/RepoList/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { 
+  List,
+  ListItem,
+} from 'react-native-elements';
+
+export default class RepoList extends Component {
+  render() {
+    return (
+      <List>
+      </List>
+    );
+  }
+}
