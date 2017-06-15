5320a82b5529b2d14bf74e6cae0c9d75faac6b03
Marcus Sanatan <msanatan@gmail.com>
2017-06-15 18:56:28 -0400

    Add button to main layout Encapsulate the page with a view so that the search bar appears at the top, the search button right below and when data is entered the list would be populated

--- app/scenes/SearchScreen/index.js
+++ app/scenes/SearchScreen/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import RepoSearchBar from './components/RepoSearchBar';
+import RepoSearchButton from './components/RepoSearchButton';
 
 export default class SearchScreen extends Component {
   render() {
     return (
-      <RepoSearchBar />
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+      }}>
+        <RepoSearchBar />
+        <RepoSearchButton />
+      </View>
     );
   }
 }
