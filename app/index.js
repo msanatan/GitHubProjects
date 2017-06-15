5b7d82dc52b5c4fdb114f4420507fb3dd3e14a24
Marcus Sanatan <msanatan@gmail.com>
2017-06-15 15:15:16 -0400

    Setup Navigation for app

--- app/index.js
+++ app/index.js
@@ -1,45 +1,12 @@
-import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  StackNavigator,
+} from 'react-navigation';
+import SearchScreen from './scenes/SearchScreen';
 
-export default class GitHubProjects extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const GitHubProjects = StackNavigator({
+  Search: { screen: SearchScreen },
 });
+
+export default GitHubProjects;
 
