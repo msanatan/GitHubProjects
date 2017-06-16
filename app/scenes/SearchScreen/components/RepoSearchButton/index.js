c1289b14622e7320cec6091a24a8b0bb4d40f8c3
Marcus Sanatan <msanatan@gmail.com>
2017-06-15 22:47:09 -0400

    Fetch API results when Search is pressed The button uses the github service and takes sends the result back to the search screen

--- app/scenes/SearchScreen/components/RepoSearchButton/index.js
+++ app/scenes/SearchScreen/components/RepoSearchButton/index.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import { Button } from 'react-native-elements';
+import { getRepos } from '../../../../services/github';
 
 export default class RepoSearchButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handlePress = this.handlePress.bind(this);
+  }
+
   render() {
     return (
       <Button 
         raised
         textStyle={{textAlign: 'center'}}
-        title='Search'/>
+        title='Search'
+        onPress={this.handlePress} />
     );
+  }
+
+  handlePress() {
+    getRepos(this.props.username)
+      .then((jsonData) => {
+        this.props.onPress(jsonData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
 
