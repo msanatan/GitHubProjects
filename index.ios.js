a159385e81c050fab926fc7d075b42ba1f592ea7
Marcus Sanatan <msanatan@gmail.com>
2017-06-16 20:23:48 -0400

    Reference src to start app because of rename

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import GitHubProjects from './app/index';
+import GitHubProjects from './src';
 
 AppRegistry.registerComponent('GitHubProjects', () => GitHubProjects);
 
